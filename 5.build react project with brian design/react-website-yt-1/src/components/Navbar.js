e01d31cd95ef68416129307cf8c38521061f7e09
mrbunroeun <bunroeunhas@gmail.com>
2025-09-06 10:50:17 +0700

    Add files via upload

---
+++ 5.build react project with brian design/react-website-yt-1/src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './Navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false);
+    } else{
+      setButton(true);
+    }
+  };
+  window.addEventListener('resize', showButton);  
+    
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          TRVL <i className="fab fa-typo3"></i>
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-link" onClick={closeMobileMenu}>
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/products" className="nav-link" onClick={closeMobileMenu}>
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link  to="/signup" >
+            <button className="signup">
+              Sign Up
+            </button>
+            </Link>
+          </li>
+        </ul>
+        {/* {button && <button className="sign-up" buttonStyle="btn--outline">sign up</button>} */}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
